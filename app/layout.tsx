5d6fbdbd0680541353740472cdf6a69c2bd5514b
Nishtha Bhan <nishthabhan@Nishthas-MacBook-Air.local>
2026-08-24 11:24:50 +0100

    update link preview text

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ const petitFormalScript = Petit_Formal_Script({
 });
 
 export const metadata: Metadata = {
-  title: "Nishtha & Wouter | RSVP",
-  description: "RSVP for the wedding of Nishtha and Wouter, 14-15 November 2027",
+  title: "FishfoundherWater| Wedding ",
+  description: "Everything you need for the wedding of Nishtha and Wouter, 14-15 November 2027",
 };
 
 export default function RootLayout({
